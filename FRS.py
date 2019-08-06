c9c0fb874540e4cfc3b68209f3dfa6b0fc362ad3
Tyler RC <tylrrc@gmail.com>
2019-08-06 16:07:00 -0500

    Create FRS.py

---
+++ FRS.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+__author__ = "Tyler Cummings"
+__copyright__ = "Nope!"
+__credits__ = "Tyler Cummings"
+__license__ = "GPL"
+__version__ = "1.0.1"
+__maintainer__ = "Rob Knight"
+__email__ = "tylrrc@gmail.com"
+__status__ = "Under construction"
+
+print ("Welcome to the Film Recommendation System!")
+print ("Please enter a film on which to base recommendations: ")
