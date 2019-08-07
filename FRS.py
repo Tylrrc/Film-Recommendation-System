0561cb55d440e7ecbdf36d606829615867f11ff4
Tyler RC <tylrrc@gmail.com>
2019-08-07 13:31:11 -0500

    Update FRS.py
    Displays results that are strongly correlated to user input. Results appear higher in list if they have more votes. Correlation between highly rated films and films with the most ratings.

--- FRS.py
+++ FRS.py
@@ -1,15 +1,44 @@
-#!/usr/bin/env python
-
-__author__ = "Tyler Cummings"
+__author__ = "T Murder"
 __copyright__ = "Nope!"
-__credits__ = "Tyler Cummings"
+__credits__ = "T Murder"
 __license__ = "GPL"
 __version__ = "1.0.1"
 __maintainer__ = "Rob Knight"
 __email__ = "tylrrc@gmail.com"
 __status__ = "Under construction"
 
-print ("Welcome to the Film Recommendation System!")
-choice = raw_input("Please enter a film on which to base recommendations: ")
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
 
-print ("Base film: " + choice)
+sns.set_style('dark')
+warnings.filterwarnings('ignore')
+
+movies = pd.read_csv("filmData/ml-latest-small/movies.csv")
+
+ratings = pd.read_csv("filmData/ml-latest-small/ratings.csv")
+
+movie_ratings_combo = pd.merge(movies, ratings, on='movieId')
+
+ratings_mean_count = pd.DataFrame(movie_ratings_combo.groupby('title')['rating'].mean())
+
+ratings_mean_count['rating_counts'] = pd.DataFrame(movie_ratings_combo.groupby('title')['rating'].count())
+
+UMR = movie_ratings_combo.pivot_table(index='userId', columns='title', values='rating')
+
+selection = input("Enter a film on which to base recommendations: ")
+
+selection_row = UMR[selection]
+
+similar = UMR.corrwith(selection_row)
+
+corrFG = pd.DataFrame(similar, columns=['Correlation'])
+corrFG.dropna(inplace=True)
+
+corrFG = corrFG.join(ratings_mean_count['rating_counts'])
+
+print(corrFG[corrFG ['rating_counts']>50].sort_values('Correlation', ascending=False).head(25))
+
+
+
